431f629691cebae0e78e1c320ab50a704ec274ab
Michael Schilling <mschilling@arkid.nl>
2017-02-24 23:19:58 +0100

    Simple service with pin logger

--- server.js
+++ server.js
@@ -11,3 +11,16 @@ firebase.initializeApp({
 });
 
 const ref = firebase.database().ref();
+
+ref.child('gate/open').on('child_added', onCodeEntered);
+
+function onCodeEntered(snapshot) {
+  const log = snapshot.val();
+  return logEntry(log)
+    .then(() => snapshot.ref.remove());
+}
+
+function logEntry(obj) {
+  obj.log_time = moment().valueOf();
+  return ref.child('history').push(obj);
+}
