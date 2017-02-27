48ec2cf4285a44b19e1e8332ea18da26c7ee0e3c
Michael Schilling <mschilling@arkid.nl>
2017-02-27 21:50:50 +0100

    Integrate Twillio service and make outgoing calls to gate phone service - issue #1

--- config/default.js
+++ config/default.js
@@ -2,6 +2,12 @@
 
 // More info: // REF: http://goenning.net/2016/05/13/how-i-manage-application-configuration-with-nodejs/
 module.exports = {
+  twilio: {
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+    authToken: process.env.TWILIO_TOKEN,
+    number: process.env.TWILIO_NUMBER,
+    gateNumber: process.env.GATE_NUMBER
+  },
   firebase: {
     databaseURL: process.env.FIREBASE_DB_URL,
     serviceAccount: process.env.FIREBASE_SERVICE_ACCOUNT
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require('dotenv').config({ silent: true });
 const config = require('./config');
 const moment = require('moment');
 
+const twilio = require('twilio');
+const client = new twilio.RestClient(config.twilio.accountSid, config.twilio.authToken);
+
 const firebase = require('firebase');
 firebase.initializeApp({
   serviceAccount: config.firebase.serviceAccount,
@@ -11,16 +14,63 @@ firebase.initializeApp({
 });
 
 const ref = firebase.database().ref();
+let appSettings = {};
 
-ref.child('gate/open').on('child_added', onCodeEntered);
+ref.child('settings').once('value').then((snapshot) => {
+  appSettings = snapshot.val();
+
+  ref.child('settings').on('child_changed', function(snapshot) {
+    appSettings[snapshot.key] = snapshot.val();
+  });
+
+  ref.child('gate/open').on('child_added', onCodeEntered);
+});
 
 function onCodeEntered(snapshot) {
-  const log = snapshot.val();
-  return logEntry(log)
-    .then(() => snapshot.ref.remove());
-}
+    const data = snapshot.val();
+    return logEntry(data)
+      .then(() => processEntry(snapshot))
+      .then(() => snapshot.ref.remove());
+  }
 
 function logEntry(obj) {
-  obj.log_time = moment().valueOf();
-  return ref.child('history').push(obj);
-}
+    obj.timestamp = moment().valueOf();
+    console.log(obj);
+    return ref.child('history').push(obj);
+  }
+
+function processEntry(snapshot) {
+    console.log('Pin correct so lets open gate', snapshot.val());
+    if (appSettings.enabled === true) {
+      return ref.child(`codes/${snapshot.val().code}`)
+        .once('value').then((codeSnapshot) => {
+          const validate = codeSnapshot.val();
+          if(validate.debug == true) {
+            return Promise.resolve();
+          }
+          return openGateUsingOutgoingCall();
+        });
+    }
+    console.log('App disabled');
+    return Promise.resolve();
+  }
+
+function openGateUsingOutgoingCall() {
+    const voiceUrl = 'http://api.slagboom.michaelschilling.com/voice/default-message';
+
+    return client.calls.create({
+      url: voiceUrl,
+      to: config.twilio.gateNumber,
+      from: config.twilio.number,
+      method: 'GET',
+      fallbackMethod: 'GET',
+      statusCallbackMethod: 'GET',
+      record: false,
+      timeout: 5
+
+    }, function (err, message) {
+      if (err) {
+        console.log(err, message.sid);
+      }
+    });
+  }
